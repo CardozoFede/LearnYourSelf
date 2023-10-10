18838810d29cf6c14c94e70f1fd9f16a968d51b3
CardozoFede <fdcardozo178@gmail.com>
2023-10-10 20:43:18 -0300

    init commit

--- app/src/main/java/com/example/learnyourself/MainActivity.java
+++ app/src/main/java/com/example/learnyourself/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.learnyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_login;
+    EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        btn_login = findViewById(R.id.button);
+
     }
+
+    public void registrarse (View x)
+    {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+
+
+    }
+
 }
